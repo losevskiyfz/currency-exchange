8ff240e8b3ff2c1c3a79db89f2073761bf78a676
Fyodor Losevskiy <losevskiyfz@gmail.com>
2025-03-23 23:42:39 +0300

    9 - Configured archive name

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,3 +29,7 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.war{
+    archiveFileName.set("ROOT.war")
+}
---
+++ src/main/java/com/github/losevskiyfz/config/CorsFilter.java
@@ -0,0 +1,35 @@
+package com.github.losevskiyfz.config;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        httpResponse.setHeader("Access-Control-Max-Age", "3600");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
