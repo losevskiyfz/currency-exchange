cd93af63a18c85d9f36450b44856079d7b0f3856
Fyodor Losevskiy <losevskiyfz@gmail.com>
2025-04-28 00:45:48 +0300

    feat(CUR-4): implemented connection pool

--- src/main/java/com/github/losevskiyfz/listener/ServletContextInitializedListener.java
+++ src/main/java/com/github/losevskiyfz/listener/ServletContextInitializedListener.java
@@ -2,19 +2,28 @@ package com.github.losevskiyfz.listener;
 
 import com.github.losevskiyfz.cdi.ApplicationContext;
 import com.github.losevskiyfz.conf.PropertiesProvider;
+import com.github.losevskiyfz.repository.pool.ConnectionPool;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.sql.SQLException;
+import java.util.Properties;
+
 @WebListener
 public class ServletContextInitializedListener implements ServletContextListener {
     private static final Logger LOG = LogManager.getLogger(ServletContextInitializedListener.class);
     private final ApplicationContext context = ApplicationContext.getInstance();
 
-    private void initializeApplicationContext() {
+    private void initializeApplicationContext() throws SQLException, ClassNotFoundException {
         context.register(PropertiesProvider.class, new PropertiesProvider());
+        Class.forName(context.resolve(Properties.class).getProperty("main.datasource.driver"));
+        context.register(ConnectionPool.class, new ConnectionPool(
+                context.resolve(PropertiesProvider.class).get("main.datasource.url"),
+                Integer.parseInt(context.resolve(PropertiesProvider.class).get("main.datasource.pool.size"))
+        ));
     }
 
     @Override
