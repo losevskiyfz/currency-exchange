d70e59efcdd29e2a33ef6ef0a05867ffc7210643
Fyodor Losevskiy <losevskiyfz@gmail.com>
2025-04-27 17:06:53 +0300

    feat(CUR-1): implemented CDI

---
+++ src/main/java/com/github/losevskiyfz/listener/ServletContextInitializedListener.java
@@ -0,0 +1,29 @@
+package com.github.losevskiyfz.listener;
+
+import com.github.losevskiyfz.cdi.ApplicationContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@WebListener
+public class ServletContextInitializedListener implements ServletContextListener {
+    private static final Logger LOG = LogManager.getLogger(ServletContextInitializedListener.class);
+    private final ApplicationContext context = ApplicationContext.getInstance();
+
+    private void initializeApplicationContext() {
+
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            initializeApplicationContext();
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        LOG.info("Application context is initialized");
+    }
+}
